2570ae0eae7d57a3b7f62a280d0aaa8191398f17
henrikaspov <henrikas.povilaitis@gmail.com>
2021-11-12 17:26:44 +0200

    project commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:250,
+      nav:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:3
+          }
+      }
+  });
+});
